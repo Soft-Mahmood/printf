6dcef9ccb6bad4f6ad67a98f8c3647de2ef8f65b
Soft_Mahmood <softmahmood@gmail.com>
2023-06-26 02:09:46 -0700

    fumctions

---
+++ functions.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_char prints a chahacter
+ * @list is the list of arguments
+ * returns the amount of characters printed
+ */
+
+int print_char(va_list list)
+{
+	_write_char(va_arg(list, int));
+	return (1);
+}
+
+/**
+ * print_string prints a string
+ * returns the number of characters printed
+ */
+
+int print_string(va_list list)
+{
+	int i;
+	char *str;
+	
+	str = va_arg(list, char *);
+	if (str == NULL)
+		str = "(NULL)";
+	for (i = 0; str[i] != '\0'; i++)
+		_write_char(str[i]);
+	return (i);
+}
+
+/**
+ * print_percent prints the % symbol
+ * returns the number of characters printed
+ */
+
+int print_percent(__attribute__((unused))va_list list)
+{
+	_write_char('%');
+	return(1);
+}
