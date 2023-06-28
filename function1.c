006f5b7d95692d8546c46aa2d97610ab4172cf55
Soft_Mahmood <softmahmood@gmail.com>
2023-06-28 12:54:18 -0700

    new main.h

---
+++ function1.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_integer prints an integer
+ * @list is the list of all arguments
+ * Returns the number printed
+ */
+
+int print_integer(va_list list)
+{
+	int num_length;
+
+	num_length = print_number(list);
+	return (num_length);
+}
+
+/**
+ * unsigned_integer - Prints Unsigned integers
+ * @list: List of all of the argumets
+ * Return: a count of the numbers
+ */
+int unsigned_integer(va_list list)
+{
+	unsigned int num;
+
+	num = va_arg(list, unsigned int);
+
+	if (num == 0)
+		return (print_unsgined_number(num));
+
+	if (num < 1)
+		return (-1);
+	return (print_unsgined_number(num));
+}
